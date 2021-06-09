ad187b526df639998f605525f48afea658c99d8e
mhz1123 <muhammadhuzefazakir@gmail.com>
2021-06-09 23:44:59 +0530

    Completed! Just final touches remaining(funtionally complete)

--- first/views.py
+++ first/views.py
@@ -28,11 +28,11 @@ def index(request):
 
 	containers = page_soup.findAll("h3")
 
-	
+
 
 	head_list = []
 	data_list = []
-	# link_list = []
+	link_list = []
 	for container in containers:
 		head_list.append(container.text)
 
@@ -44,9 +44,10 @@ def index(request):
 
 	len_head_list = len(head_list)-1
 
-	# links=page_soup.find_all("h3.a")
-	# for link in links:
-	# 	link_list.append(link.text)
+	links=page_soup.find_all("h3",{"class": "latest-head"})
+	
+	for link in links:
+		link_list.append(f"https://www.sciencedaily.com{link.a['href']}")
 
 	scraped_res = {}
 	for key in head_list:
@@ -54,13 +55,94 @@ def index(request):
 			scraped_res[key] = value
 			data_list.remove(value)
 			break  
+
+	for data in summary:
+		data_list.append(data.text)
 	
+	head_list1=head_list[1]
+	head_list2=head_list[2]
+	head_list3=head_list[3]
+	head_list4=head_list[4]
+	head_list5=head_list[5]
+	head_list6=head_list[6]
+	head_list7=head_list[7]
+	head_list8=head_list[8]
+	head_list9=head_list[9]
+	head_list10=head_list[10]
+	head_list11=head_list[11]
+	head_list12=head_list[12]
+	data_list1=data_list[1]
+	data_list2=data_list[2]
+	data_list3=data_list[3]
+	data_list4=data_list[4]
+	data_list5=data_list[5]
+	data_list6=data_list[6]
+	data_list7=data_list[7]
+	data_list8=data_list[8]
+	data_list9=data_list[9]
+	data_list10=data_list[10]
+	data_list11=data_list[11]
+	data_list12=data_list[12]
+
+	link_list1=link_list[1]
+	link_list2=link_list[2]
+	link_list3=link_list[3]
+	link_list4=link_list[4]
+	link_list5=link_list[5]
+	link_list6=link_list[6]
+	link_list7=link_list[7]
+	link_list8=link_list[8]
+	link_list9=link_list[9]
+	link_list10=link_list[10]
+	link_list11=link_list[11]
+	link_list12=link_list[12]
+
 		
 	return render(request, "first/index.html",{
 		"head_list":head_list,
 		"scraped_res":scraped_res,
 		"data_list":data_list,
 		"len_head_list":len_head_list,
+
+		"head_list1":head_list1,
+		"head_list2":head_list2,
+		"head_list3":head_list3,
+		"head_list4":head_list4,
+		"head_list5":head_list5,
+		"head_list6":head_list6,
+		"head_list7":head_list7,
+		"head_list8":head_list8,
+		"head_list9":head_list9,
+		"head_list10":head_list10,
+		"head_list11":head_list11,
+		"head_list12":head_list12,
+
+		"data_list1":data_list1,
+		"data_list2":data_list2,
+		"data_list3":data_list3,
+		"data_list4":data_list4,
+		"data_list5":data_list5,
+		"data_list6":data_list6,
+		"data_list7":data_list7,
+		"data_list8":data_list8,
+		"data_list9":data_list9,
+		"data_list10":data_list10,
+		"data_list11":data_list11,
+		"data_list12":data_list12,
+
+		"link_list1":link_list1,
+		"link_list2":link_list2,
+		"link_list3":link_list3,
+		"link_list4":link_list4,
+		"link_list5":link_list5,
+		"link_list6":link_list6,
+		"link_list7":link_list7,
+		"link_list8":link_list8,
+		"link_list9":link_list9,
+		"link_list10":link_list10,
+		"link_list11":link_list11,
+		"link_list12":link_list12,
+
 		# "link_list":link_list,
 	})
 
